575dd318fa8b3f9e275bb248c2d097d3b445e55b
adityapratama09 <1224160041@global.ac.id>
2026-03-03 15:48:13 +0700

    pertemuan 3(update tempalte)
    3 maret 2026

---
+++ application/views/templates/sidebar.php
@@ -0,0 +1,18 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
+        <div class="sidebar-brand-text mx-3">Perpustakaan</div>
+
+</a>
+<li class="nav-item">
+    <a class="nav-link" href="<?= site_url('kategori'); ?>">
+        <i class="fas fa-fw fa-folder"></i>
+        <span>kategori</span>
+</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="<?= site_url('buku'); ?>">
+        <i class="fas fa-fw fa-book"></i>
+        <span>buku</span>
+ </a>
+</li>
+</ul>       
